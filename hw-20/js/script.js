c52cc5ef672dc94a73ca63a5e8c37665cf467166
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-06-13 16:45:22 +0300

    changed variables names

--- hw-20/js/script.js
+++ hw-20/js/script.js
@@ -8,58 +8,58 @@
 
 //2
 function counterFunc(...args) {
-  //var 1
-  // let result = 0
-  
-  // for (k in args) {
-  //   result ++
-  // }
+	//var 1
+	// let result = 0
 
-  // return result;
+	// for (k in args) {
+	//   result ++
+	// }
 
-  //var 2
-  return args.length
+	// return result;
+
+	//var 2
+	return args.length;
 }
 
 console.log(counterFunc(2, 3, 4, 5));
 
 //3
 function sortFunc(a, b) {
-  if (a > b) {
-    return 1
-  } else if (a < b) {
-    return -1
-  } else {
-    return 0
-  }
+	if (a > b) {
+		return 1;
+	} else if (a < b) {
+		return -1;
+	} else {
+		return 0;
+	}
 }
 
-console.log(sortFunc(1 , 3))
-console.log(sortFunc(3 , 3))
-console.log(sortFunc(10 , 3))
+console.log(sortFunc(1, 3));
+console.log(sortFunc(3, 3));
+console.log(sortFunc(10, 3));
 
 // 4
 function factorial(a) {
-  let result = 1;
-  
-  for(let i = a; i != 0; i--) {
-    result *= i
-  }
-  
-  return result
+	let result = 1;
+
+	for (let i = a; i != 0; i--) {
+		result *= i;
+	}
+
+	return result;
 }
 
 console.log(factorial(5));
 
 //5
 function concat(...args) {
-  let result = ''
+	let result = '';
 
-  for (k in args) {
-    result += args[k]
-  }
+	for (k in args) {
+		result += args[k];
+	}
 
-  return result
+	return result;
 }
 
 console.log(concat(1, 2, 3));
@@ -67,31 +67,31 @@ console.log(concat(1, 2, 3, 4, 5));
 
 //6
 function square(a, b = false) {
-  return b ? a * b : a ** 2
+	return b ? a * b : a ** 2;
 }
 
 console.log(square(2, 6));
 console.log(square(4));
 
 //normal
-//1 
-function isPerfectNum(a) {
-  let summOfDividers = 0;
+//1
+function isNumPerfect(a) {
+	let dividersSum = 0;
 
-  for (let i = 0; i < a; i++ ) {
-    a % i == 0 ? summOfDividers += i : false;
-  }
+	for (let i = 0; i < a; i++) {
+		a % i == 0 ? (dividersSum += i) : false;
+	}
 
-  return summOfDividers == a ? true : false;
+	return dividersSum == a ? true : false;
 }
 
-console.log(isPerfectNum(6));
+console.log(isNumPerfect(6));
 
 //2
-function showPerfectNumsInFange(min, max) {
-  for (let i = min; i <= max; i++) {
-    isPerfectNum(i) ? console.log(`${i} is perfect`) : false;
-  }
+function showPerfectNumsInRange(min, max) {
+	for (let i = min; i <= max; i++) {
+		isNumPerfect(i) ? console.log(`${i} is perfect`) : false;
+	}
 }
 
-showPerfectNumsInFange(1,10000)
+showPerfectNumsInRange(1, 10000);
