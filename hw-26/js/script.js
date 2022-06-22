ddbc0fc6c27cfb899de6336c560c8da6a695ab99
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-06-22 14:40:15 +0300

    done hero

--- hw-26/js/script.js
+++ hw-26/js/script.js
@@ -5,3 +5,12 @@ burger.addEventListener('click', (event) => {
 	burger.classList.toggle('menu--active');
 	nav.classList.toggle('nav--active');
 });
+
+new Splide('#hero-splide', {
+	type: 'loop',
+	perPage: 1,
+	autoplay: true,
+	interval: 2500,
+	cover: true,
+	arrows: false,
+}).mount();
