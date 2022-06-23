58465ac873b0d1a5bad3789852b077be5e822c94
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-06-24 01:03:42 +0300

    done new-arrival section

--- hw-26/js/script.js
+++ hw-26/js/script.js
@@ -14,3 +14,24 @@ new Splide('#hero-splide', {
 	cover: true,
 	arrows: false,
 }).mount();
+
+new Splide('#new-arrivals', {
+	type: 'loop',
+	perPage: 5,
+	perMove: 1,
+	autoplay: true,
+	interval: 2500,
+	gap: '20px',
+	pagination: false,
+	breakpoints: {
+		768: {
+			perPage: 1,
+			arrows: false,
+			pagination: true,
+		},
+		1200: {
+			perPage: 3,
+			gap: '15px',
+		},
+	},
+}).mount();
